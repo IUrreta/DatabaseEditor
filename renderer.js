652bba8bc7b2e50a28f4165e56b17874d6be62c4
Ignacio <ignacioutato@gmail.com>
2023-07-27 22:04:05 +0200

    drag 2 to one team done

--- renderer.js
+++ renderer.js
@@ -35,33 +35,35 @@ document.addEventListener('DOMContentLoaded', function () {
                 target.setAttribute('data-y', y);
             },
             end(event) {
-                const target = event.target;
-                const contractedDrivers = document.getElementById('contracted-drivers');
-                const contractedRect = contractedDrivers.getBoundingClientRect();
-                const freeDrivers = document.getElementById('free-drivers');
-                const freeRect = freeDrivers.getBoundingClientRect();
-        
-                const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-                const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-        
-                // Verifica si el punto final del evento de soltar está dentro de los límites del div "contracted-drivers"
-                if (event.clientX >= contractedRect.left && event.clientX <= contractedRect.right &&
-                    event.clientY >= contractedRect.top && event.clientY <= contractedRect.bottom) {
-                  // Suelta el div en el div "contracted-drivers"
-                  contractedDrivers.appendChild(target);
-                }
-        
-                // Verifica si el punto final del evento de soltar está dentro de los límites del div "free-drivers"
-                if (event.clientX >= freeRect.left && event.clientX <= freeRect.right &&
-                    event.clientY >= freeRect.top && event.clientY <= freeRect.bottom) {
-                  // Suelta el div en el div "free-drivers"
-                  freeDrivers.appendChild(target);
-                }
-        
-                // Reinicia las coordenadas de arrastre
-                target.style.transform = 'none';
-                target.setAttribute('data-x', 0);
-                target.setAttribute('data-y', 0);
+                        const target = event.target;
+        const contractedDrivers = document.getElementById('contracted-drivers');
+        const contractedRect = contractedDrivers.getBoundingClientRect();
+        const freeDrivers = document.getElementById('free-drivers');
+        const freeRect = freeDrivers.getBoundingClientRect();
+
+        const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+        const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+        // Verifica si el punto final del evento de soltar está dentro de los límites del div "contracted-drivers"
+        if (event.clientX >= contractedRect.left && event.clientX <= contractedRect.right &&
+            event.clientY >= contractedRect.top && event.clientY <= contractedRect.bottom) {
+          // Suelta el div en el div "contracted-drivers"
+          if (contractedDrivers.childElementCount < 2) {
+            contractedDrivers.appendChild(target);
+          }
+        }
+
+        // Verifica si el punto final del evento de soltar está dentro de los límites del div "free-drivers"
+        if (event.clientX >= freeRect.left && event.clientX <= freeRect.right &&
+            event.clientY >= freeRect.top && event.clientY <= freeRect.bottom) {
+          // Suelta el div en el div "free-drivers"
+          freeDrivers.appendChild(target);
+        }
+
+        // Reinicia las coordenadas de arrastre
+        target.style.transform = 'none';
+        target.setAttribute('data-x', 0);
+        target.setAttribute('data-y', 0);
             }
         }
     });
