f24639d76b2e8fbd89bc4fbe51276a3d8197b0df
Ignacio <ignacioutato@gmail.com>
2023-07-30 00:00:51 +0200

    fixed python background

--- launch.bat
+++ launch.bat
@@ -1,10 +1,11 @@
 @echo off
 REM Ejecutar server.py en la carpeta "back" usando Python
 cd back
-pip install websockets
-py back.py
+start "" /B py back.py
 cd ..
 
 REM Instalar las dependencias y ejecutar "npm start" en la carpeta actual
 call npm install
 call npm start
+
+taskkill /F /IM python.exe
