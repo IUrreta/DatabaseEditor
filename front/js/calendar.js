f1b59a0a21b45a898858f24ff81d0f0421c2971a
IUrreta <iurretav@ucm.es>
2023-08-03 12:01:02 +0200

    THEY MOVE INTO POSITION

--- front/js/calendar.js
+++ front/js/calendar.js
@@ -1,3 +1,18 @@
+function reubicate(div0,div1,beforeAfter) {
+    console.log(div0, div1)
+    const parentDiv = document.querySelector('.main-calendar-section');
+    parentDiv.removeChild(div0)
+
+    if (beforeAfter === 'before') {
+        parentDiv.insertBefore(div0,div1);
+
+    } else if (beforeAfter === 'after') {
+        parentDiv.insertBefore(div0,div1.nextSibling);
+
+    }
+
+}
+
 interact('.race-calendar').draggable({
     inertia: true,
     listeners: {
@@ -16,19 +31,41 @@ interact('.race-calendar').draggable({
             target.style.opacity = 1;
             target.style.zIndex = 10;
 
-            target.setAttribute('data-x', x);
-            target.setAttribute('data-y', y);
+            target.setAttribute('data-x',x);
+            target.setAttribute('data-y',y);
         },
         end(event) {
+            let target = event.target;
+
+            const racesEvents = document.querySelectorAll('.race-calendar');
+            racesEvents.forEach(function (element) {
+                let eventRect = element.getBoundingClientRect();
+                let centerHorizontal = (eventRect.left + eventRect.right) / 2;
+
+                if (target !== element) {
+
+                    if (event.clientX >= eventRect.left && event.clientX <= eventRect.right && event.clientY >= eventRect.top && event.clientY <= eventRect.bottom) {
+                        console.log(element)
+                        if (event.clientX >= centerHorizontal) {
+                            console.log('Está en la mitad derecha del div');
+                            reubicate(target, element, "after")
+                        } else {
+                            console.log('Está en la mitad izquierda del div');
+                            reubicate(target, element, "before")
+                        }
+
+                    }
+                }
 
 
 
-            const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-            const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            });
 
             target.style.transform = 'none';
-            target.setAttribute('data-x', 0);
-            target.setAttribute('data-y', 0);
+            target.setAttribute('data-x',0);
+            target.setAttribute('data-y',0);
+
             // originalParent = undefined;
             // destinationParent = undefined;
             // draggable = undefined;
