29e6be00c84aff37ebe79f1f8593ff5d52f3f2fc
IUrreta <ignacio010601@gmail.com>
2023-08-21 17:22:33 +0200

    fixed space and version check

--- front/js/renderer.js
+++ front/js/renderer.js
@@ -177,9 +177,13 @@ document.addEventListener('DOMContentLoaded',function () {
                             isSame = false;
                         }
                         else {
+                            console.log(latestVer)
                             for (let i = 0; i < latestVer.length; i++) {
                                 if (latestVer[i] > actualVer[i]) {
                                     isSame = false;
+                                    break;
+                                }
+                                else if(latestVer[i] < actualVer[i]){
                                     break;
                                 }
                             }
