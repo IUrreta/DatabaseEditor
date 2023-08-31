e4431dacde4633deb4f6a33e604c6ad5d94ad15c
Ignacio <ignacioutato@gmail.com>
2023-08-31 20:23:38 +0200

    decent style

--- front/js/seasonViewer.js
+++ front/js/seasonViewer.js
@@ -3,25 +3,48 @@ let seasonTable;
 let races_ids = []
 
 function createTable(calendar) {
-    calendar.forEach(function(elem, index){
+    calendar.forEach(function (elem, index) {
         races_ids.push(calendar[index][0])
     })
     console.log(races_ids)
     seasonTable = new Tabulator("#seasonresults-table", {
         layout: "fitColumns",
-        columns: [{ title: "Driver", field: "driver", width: 175, headerSort: false },
+        maxWidth: "1650px",
+        responsiveLayout:"hide",
+        columns: [{ title: "Driver", field: "driver", width: 175, headerSort: false, resizable:false, formatter: "html"},
         ...calendar.map((race, index) => ({
             title: '<span class="flag-header"><img src="' + codes_dict[races_map[race[1]]] + '" alt="Image 1"></span>',
             field: "race" + race[0],
             hozAlign: "center",
             headerSort: false,
+            resizable:false
         })),
         {
             title: "Points",
             field: "points",
             hozAlign: "center",
             headerSort: false,
+            resizable:false
         }],
+        rowFormatter: function(row) {
+            var rowData = row.getData();
+            
+            for (var key in rowData) {
+                if (key !== "driver" && key !== "points") {
+                    var cellValue = rowData[key];
+                    
+                    if (cellValue == 25) {
+                        row.getCell(key).getElement().style.backgroundColor = "gold";
+                        row.getCell(key).getElement().style.color = "#18152e";
+                    } else if (cellValue == 18) {
+                        row.getCell(key).getElement().style.backgroundColor = "silver";
+                        row.getCell(key).getElement().style.color = "#18152e";
+                    } else if (cellValue == 15) {
+                        row.getCell(key).getElement().style.backgroundColor = "#cd7f32";
+                    }
+                }
+            }
+        }
     });
 }
 
@@ -33,13 +56,26 @@ function loadTable(allDrivers) {
 }
 
 function addDriver(driverInfo) {
-    let driverName = driverInfo[0]
-    let rowData = { driver: driverName };
+    let nameDiv = document.createElement("div");
+    let name = driverInfo[0].split(" ")
+    let spanName = document.createElement("span")
+    let spanLastName = document.createElement("span")
+    spanName.textContent = name[0] + " "
+    spanLastName.textContent = " "+ name[1].toUpperCase()
+    spanLastName.classList.add("bold-font")
+    spanLastName.dataset.teamid = driverInfo[1]
+    nameDiv.appendChild(spanName)
+    nameDiv.appendChild(spanLastName)
+    manageColor(spanLastName, spanLastName)
+    
+    let rowData = { driver: nameDiv.innerHTML };
     driverInfo.slice(2).forEach((pair, index) => {
         rowData["race" + pair[0]] = pair[2];
     });
+
+
     let totalPoints = 0;
-    driverInfo.slice(2).forEach(function(elem){
+    driverInfo.slice(2).forEach(function (elem) {
         totalPoints += elem[2]
     })
     rowData["points"] = totalPoints;
