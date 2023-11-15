63d7da61570a6e81a9d1dcb9d0f2074ec9cf9db7
IUrreta <ignacioutato@gmail.com>
2023-11-15 15:42:29 +0100

    objectives done

--- front/js/teams.js
+++ front/js/teams.js
@@ -5,3 +5,26 @@ document.querySelector("#teamMenu").querySelectorAll("a").forEach(function(elem)
     })
     
 })
+
+document.querySelector("#objectiveMenu").querySelectorAll("a").forEach(function(elem){
+    elem.addEventListener("click", function(){
+        document.querySelector("#objectiveButton").innerText = elem.textContent
+    })
+    
+})
+
+document.querySelector("#objAndYear").querySelector(".bi-plus-lg").addEventListener("click", function(){
+    document.querySelector("#longTermInput").value = Number(document.querySelector("#longTermInput").value) + 1
+})
+
+document.querySelector("#objAndYear").querySelector(".bi-dash-lg").addEventListener("click", function(){
+    document.querySelector("#longTermInput").value = Number(document.querySelector("#longTermInput").value) - 1
+})
+
+document.querySelector("#seasonObjective").querySelector(".bi-plus-lg").addEventListener("click", function(){
+    document.querySelector("#seasonObjectiveInput").value = Number(document.querySelector("#seasonObjectiveInput").value) + 1
+})
+
+document.querySelector("#seasonObjective").querySelector(".bi-dash-lg").addEventListener("click", function(){
+    document.querySelector("#seasonObjectiveInput").value = Number(document.querySelector("#seasonObjectiveInput").value) - 1
+})
