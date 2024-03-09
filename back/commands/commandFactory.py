68bab1a9eceffd195d839718ad0e5b382b24e29e
Ignacio <ignacioutato@gmail.com>
2024-03-09 11:37:23 +0100

    added ocmmand dict so factory is more pretty

--- back/commands/commandFactory.py
+++ back/commands/commandFactory.py
@@ -32,58 +32,15 @@ class CommandFactory:
                     "yearSelectedPrediction": YearSelectedPredictionCommand, "yearSelectedPredictionModal": YearSelectedPredictionModalCommand, 
                     "predict": PredictCommand, "predictMontecarlo": PredictMontecarloCommand, "unretireDriver": UnretireCommand, 
                     "configUpdate": ConfigUpdateCommand}
+    
     def __init__(self):
         pass
         
     def create_command(self, message, client):
         command_type = message["command"]
         print(message)
-        if command_type == "connect":
-            return ConnectCommand(message, client)
-        elif command_type == "saveSelected":
-            return SaveSelectedCommand(message, client)
-        elif command_type == "hire":
-            return HireCommand(message, client)
-        elif command_type == "fire":
-            return FireCommand(message, client)
-        elif command_type == "requestDriver":
-            return RequestDriverCommand(message, client)
-        elif command_type == "autoContract":
-            return AutoContractCommand(message, client)
-        elif command_type == "swap":
-            return SwapCommand(message, client)
-        elif command_type == "editStats":
-            return EditStatsCommand(message, client)
-        elif command_type == "calendar":
-            return CalendarCommand(message, client)
-        elif command_type == "editContract":
-            return EditContractCommand(message, client)
-        elif command_type == "editPerformance":
-            return EditPerformanceCommand(message, client)
-        elif command_type == "editEngine":
-            return EditEngineCommand(message, client)
-        elif command_type == "yearSelected":
-            return YearSelectedCommand(message, client)
-        elif command_type == "yearSelectedH2H":
-            return YearSelectedH2HCommand(message, client)
-        elif command_type == "H2HConfigured":
-            return H2HConfiguredCommand(message, client)
-        elif command_type == "teamRequest":
-            return TeamRequestCommand(message, client)
-        elif command_type == "editTeam":
-            return EditTeamCommand(message, client)
-        elif command_type == "yearSelectedPrediction":
-            return YearSelectedPredictionCommand(message, client)
-        elif command_type == "yearSelectedPredictionModal":
-            return YearSelectedPredictionModalCommand(message, client)
-        elif command_type == "predict":
-            return PredictCommand(message, client)
-        elif command_type == "predictMontecarlo":
-            return PredictMontecarloCommand(message, client)
-        elif command_type == "unretireDriver":
-            return UnretireCommand(message, client)
-        elif command_type == "configUpdate":
-            return ConfigUpdateCommand(message, client)
-        # Aquí podrías añadir más comandos según sea necesario
+        if command_type in self.command_dict:
+            return self.command_dict[command_type](message, client)
         else:
             raise ValueError("Unknown command type")
+        
