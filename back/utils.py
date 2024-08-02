b100b2f49cbe65dcdf57bb9732b8c8a6fb34524d
Ignacio <ignacioutato@gmail.com>
2024-08-02 16:45:02 +0200

    ficed bug naitonality 23

--- back/utils.py
+++ back/utils.py
@@ -125,7 +125,7 @@ class DatabaseUtils:
                 retirement = self.fetch_driverRetirement(id)
                 race_formula = self.fetch_raceFormula(id)
                 team_future = self.fetch_for_future_contract(id)
-                country_code = self.fetch_nationality(id)
+                country_code = self.fetch_nationality(id, game_year)
                 if race_formula[0] == None:
                     race_formula = (4,)
                 data_dict = {i: result[i] for i in range(len(result))}
@@ -344,7 +344,7 @@ class DatabaseUtils:
                 superlicense = self.fetch_superlicense(id)
                 team_future = self.fetch_for_future_contract(id)
                 driver_code = self.fetch_driverCode(id)
-                country_code = self.fetch_nationality(id)
+                country_code = self.fetch_nationality(id, game_year)
                 data_dict = {i: result[i] for i in range(len(result))}
                 data_dict["driver_number"] = driver_number[0]
                 data_dict["wants1"] = driver_number[1]
@@ -368,18 +368,24 @@ class DatabaseUtils:
 
         return formatted_tuples
     
-    def fetch_nationality(self, driverID):
-        code = self.cursor.execute(f"SELECT CountryID FROM Staff_BasicData WHERE StaffID = {driverID}").fetchone()[0]
-        nationality = self.cursor.execute(f"SELECT Name FROM Countries WHERE CountryID = {code}").fetchone()[0]
-        expr = r'(?<=\[Nationality_)[^\]]+'
-        result = re.search(expr, nationality)
-        if result:
-            nat = result.group(0)
-            nat_name = re.sub(r'(?<!^)([A-Z])', r' \1', nat)
+    def fetch_nationality(self, driverID, game_year):
+        if game_year == "24":
+            code = self.cursor.execute(f"SELECT CountryID FROM Staff_BasicData WHERE StaffID = {driverID}").fetchone()[0]
+            nationality = self.cursor.execute(f"SELECT Name FROM Countries WHERE CountryID = {code}").fetchone()[0]
+            expr = r'(?<=\[Nationality_)[^\]]+'
+            result = re.search(expr, nationality)
+            if result:
+                nat = result.group(0)
+                nat_name = re.sub(r'(?<!^)([A-Z])', r' \1', nat)
+                nat_code = countries_dict.get(nat_name, "")
+                return nat_code
+            else:
+                return ""
+        elif game_year == "23":
+            nationality = self.cursor.execute(f"SELECT Nationality FROM Staff_BasicData WHERE StaffID = {driverID}").fetchone()[0]
+            nat_name = re.sub(r'(?<!^)([A-Z])', r' \1', nationality)
             nat_code = countries_dict.get(nat_name, "")
             return nat_code
-        else:
-            return ""
     
     def fetch_driverCode(self, driverID):
         code = self.cursor.execute(f"SELECT DriverCode FROM Staff_DriverData WHERE StaffID = {driverID}").fetchone()
