e92f7cee907cf6f64c03b0c8936dce46bd546ef7
Ignacio <ignacioutato@gmail.com>
2025-11-04 19:16:50 +0100

    feat(news): add driver stats to prompt, better potChamp prompt and regenerate button

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const packageJson = require('./package.json');
 
 
 module.exports = {
-  mode: 'production',
+  mode: 'production', // Cambia a 'production' para producción
 
   entry: './src/index.js',  // Archivo de entrada principal
 
